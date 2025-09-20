11fb1c03fcad66612365250db79f811ed2c86a07
Sammy Nava <sammy@sammynava.com>
2025-09-20 14:08:14 -0500

    Add TaskerItems table to Db

--- TaskerFullStack/Data/ApplicationDbContext.cs
+++ TaskerFullStack/Data/ApplicationDbContext.cs
@@ -7,5 +7,7 @@ namespace TaskerFullStack.Data
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<ImageUpload> Images { get; set; }
+
+        public DbSet<DbTaskerItem> TaskerItems { get; set; }
     }
 }
