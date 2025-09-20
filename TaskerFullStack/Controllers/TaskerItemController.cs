4d54e2d98a7aaa50cf7791de96426de9d41289e8
Sammy Nava <sammy@sammynava.com>
2025-09-20 15:53:22 -0500

    Q&D GET endpoint

--- TaskerFullStack/Controllers/TaskerItemController.cs
+++ TaskerFullStack/Controllers/TaskerItemController.cs
@@ -2,8 +2,11 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.EntityFrameworkCore;
 using TaskerFullStack.Client.Models;
 using TaskerFullStack.Data;
+using TaskerFullStack.Migrations;
 using TaskerFullStack.Models;
 
 namespace TaskerFullStack.Controllers;
@@ -19,6 +22,7 @@ public class TaskerItemController(
     private readonly UserManager<ApplicationUser> _userManager = userManager;
     private readonly ApplicationDbContext _db = db;
 
+    // post:  api/TaskerItems
     [HttpPost]
     public async Task<ActionResult<TaskerItem>> PostDbTaskerItem([FromBody] TaskerItem takserItem)
     {
@@ -27,7 +31,7 @@ public class TaskerItemController(
             Id = takserItem.Id,
             Name = takserItem.Name,
             IsComplete = takserItem.IsComplete,
-            UserId = _userManager.GetUserId(User)
+            UserId = CurrentUserId
         };
 
         _db.TaskerItems.Add(dbTaskerItem);
@@ -35,4 +39,14 @@ public class TaskerItemController(
 
         return Ok();
     }
+
+    // get:  api/TaskerItems
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TaskerItem>>> GetDbTaskerItems()
+    {
+        List<DbTaskerItem> items = await _db.TaskerItems.Where(t => t.UserId == CurrentUserId).ToListAsync();
+        return items;
+    }
+
+    private string CurrentUserId => _userManager.GetUserId(User) ?? string.Empty;
 }
